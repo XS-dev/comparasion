c7b0ace3e28c443b2c5fb0786037a109c68104fc
XS <ab2372638439@gmail.com>
2023-04-11 15:13:18 +0800

    因为感觉01010101不对劲所以又试了一次，没发现问题

--- comparison.runs/synth_1/rundef.js
+++ comparison.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log com_1010_1010.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source com_1010_1010.tcl" );
+         "-log com_0101_0101.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source com_0101_0101.tcl" );
 
 
 
